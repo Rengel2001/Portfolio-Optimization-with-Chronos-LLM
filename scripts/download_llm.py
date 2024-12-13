8d97791e5ce11d7d4b4ec4b6d8c75a334881abc5
Rengel2001 <ryan.engel30@gmail.com>
2024-12-13 16:08:36 -0500

    Added code and results

--- scripts/download_llm.py
+++ scripts/download_llm.py
@@ -11,4 +11,4 @@ os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 # Chronos-T5 model from Hugging Face
 model_path = "amazon/chronos-bolt-base"
 model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
-print(f"Chronos-T5 has been loaded successfully: {model_path}")
+print(f"Chronos-T5 has been loaded successfully: {model_path}") 
